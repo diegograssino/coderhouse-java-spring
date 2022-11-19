16c4d1c513ce1f97ec0aff1da99f6dfb2d66422e
Diego Grassino <diegograssino@gmail.com>
2022-11-19 13:22:15 -0300

    Desafío 1

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Curso Java inicial con Spring
+
+Desafíos entregables:
+- [Manejo de datos tipo float](https://breakdance.github.io/breakdance/)
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    float number = 3.14f;
+    float decimalPart = number % 1;
+    int integerPart = (int) number;
+
+    System.out.println(
+      "La variable de tipo float tiene un valor de " +
+      number +
+      ", su parte entera es " +
+      integerPart +
+      ", mientras que su parte decimal es " +
+      decimalPart
+    );
   }
 }
